aeb8f88972541ba7badec2136e5e3c1a6853f4b1
Pramatma Vishwakarma <vpramatma@gmail.com>
2024-02-03 11:16:33 +0530

    Recipe APIs Models, Views and Tests

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,63 @@
+"""
+Tests for models.
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from decimal import Decimal
+from core import models
+
+
+class ModelTests(TestCase):
+    """Test models."""
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = "test@codemon.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.check_password(password), True)
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.com', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a valueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test1234')
+
+    def test_create_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test12345'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass12345',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample Recipe Name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description'
+        )
+        self.assertEqual(str(recipe), recipe.title)
